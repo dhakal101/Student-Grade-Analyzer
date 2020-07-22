ff122e3eafa7c0d89716f42484c0faed71574ada
dhakal101 <50218546+gdhakal123@users.noreply.github.com>
2020-07-22 11:25:52 -0600

    s

--- array-helpers.js
+++ array-helpers.js
@@ -7,6 +7,16 @@
 // Purpose:         Search anArray for item.  Return true if item is in anArray, else return false.
 // Return Value:    true or false, depending on whether item is in anArray
 
+function includes(item, anArray) {
+
+    for (let i = 1; i < anArray.length; i++) {
+        if (anArray[i] === item) {
+            return 'True';
+        } else {
+            return 'False';
+        }
+    }
+}
 
 
 
@@ -16,6 +26,17 @@
 // Purpose:         Determine the minimum value in anArray.
 // Return Value:    The minimum value in anArray.
 
+function minItem(anArray) {
+
+    let firstidx = anArray;
+    let lowestvalue = firstidx[0]; // Assign First Index as the lowest
+    for (let i = 1; i < firstidx.length; i++) {
+        if (firstidx[i] < lowestvalue) { // If any other numbers are smaller it replaces the first index
+            lowestvalue = firstidx[i];
+        }
+    }
+    return lowestvalue; // Return the lowest value
+}
 
 
 
@@ -25,6 +46,17 @@
 // Purpose:         Determine the minimum value in anArray.
 // Return Value:    The mmaximum value in anArray.
 
+function maxItem(anArray) {
+
+    let firstidx = anArray;
+    let largestvalue = firstidx[0]; // Assign First Index as the largest
+    for (let i = 1; i < firstidx.length; i++) {
+        if (firstidx[i] > largestvalue) { // If any other numbers are bigger it replaces the first index
+            largestvalue = firstidx[i];
+        }
+    }
+    return largestvalue; // Return the largest value
+}
 
 
 
@@ -34,4 +66,11 @@
 // Purpose:         Determine the average of the values in anArray.
 // Return Value:    The average of the values in anArray.
 
-
+function arrayAverage(anArray) {
+    let sum = 0;      // Assign the sum to be 0
+    for (let i = 0; i < anArray.length; i++) {      // Loop through all the indexes while adding them
+        sum += anArray[i];        // Add all the indexes to the sum
+    }
+    let average = sum / anArray.length;       // Divide the sum by the number of indexes to get average
+    return average;
+}
--- math-extensions.js
+++ math-extensions.js
@@ -1,16 +1,16 @@
 // Math Object Extensions
 
-Math.randomDec = function(low, high) {
+Math.randomDec = function (low, high) {
     // Return a random decimal between low (inclusive) and high (exclusive)
     return Math.random() * (high - low) + low;
 }
 
-Math.randomInt = function(low, high) {
+Math.randomInt = function (low, high) {
     // Return a random integer between low (inclusive) and high (exclusive)
     return Math.floor(Math.randomDec(low, high));
 }
 
-Math.roundTo = function(num, numPlaces) {
+Math.roundTo = function (num, numPlaces) {
     // Round num off to the nearest numPlaces
     num = num * 10 ** numPlaces;
     num = Math.round(num);
